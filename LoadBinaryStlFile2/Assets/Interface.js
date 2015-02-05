530b30a215efe806c3c0223d916a3bc6aa346aa7
Lucas Zeer-Wanklyn <lucas.zeer@gmail.com>
2015-02-04 23:11:17 -0500

    Working demo of new renderer

---
+++ LoadBinaryStlFile2/Assets/Interface.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+public var stl_meshes : GameObject; 
+var selGridInt : int = 0;
+var selStrings : String[] = ["Grid 1", "Grid 2", "Grid 3", "Grid 4"];
+var price : int = 20;
+var selTexture : Texture[];
+var style : GUIStyle;
+
+
+function Start () {
+	style.normal.textColor = Color.black;
+}
+function OnGUI () {
+
+//	GUI.Label (Rect (10, 10, 100, 20), "Choose Texture", style);
+//	selGridInt = GUI.SelectionGrid (Rect (25, 35, 240, 40), selGridInt, selStrings, 2);
+		
+//  	var stl_file = stl_meshes.GetComponent(STLFile);
+//  	stl_meshes.renderer.material.mainTexture = selTexture[selGridInt];
+//  stl_meshes.renderer.material.color = stl_file.default_color;
+}
+	
+function Update () {
+
+}
--- UnityRails/app/controllers/player_controller.rb
+++ UnityRails/app/controllers/player_controller.rb
@@ -1,6 +1,24 @@
 class PlayerController < ApplicationController
   def grab_file_url
-  	render text: params.inspect
+  	
+  	# Get rid of all older files 
+    File3D.all.each do |f|
+    	f.stl.destroy
+    	f.save
+    	f.destroy
+    end
+    
+    # Create a new temporary stl file for viewing in Unity
+    file = File3D.new()
+    file.stl = params[:stl][:file]
+    file.save
+
+    # Get the url of the file
+    data = {
+    	file_url: file.stl.url
+    }
+
+  	render json: data.to_json, status: 200
   end
 
   def view
