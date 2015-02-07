a548c24deb56dca3a765c5284eb98b415c0aac0c
Lucas Zeer-Wanklyn <lucas.zeer@gmail.com>
2015-02-07 11:56:26 -0500

    Partial fix for spacing on same z-level

---
+++ 3DCommenting/Assets/STLFile.js
@@ -0,0 +1,4 @@
+﻿#pragma strict
+public class STLFile extends MonoBehaviour {
+    public var default_color : Color;   
+}
---
+++ 3DCommenting/Assets/TrackBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackBall : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	void OnMouseDrag ()
+	{
+		RaycastHit hit;
+		
+		if (Input.GetMouseButton(0))
+		{
+			if (Physics.Raycast (Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+			{
+				float x = -Input.GetAxis("Mouse X");
+				float y = -Input.GetAxis("Mouse Y");
+				float speed = 10;
+				transform.rotation *= Quaternion.AngleAxis(x*speed, Vector3.up);
+				transform.rotation *= Quaternion.AngleAxis(y*speed, Vector3.right);
+			}
+		}
+	}
+	
+}
